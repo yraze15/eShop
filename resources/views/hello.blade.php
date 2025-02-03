74c18ee27bda25e79a5d7495e5de67ab7d923c23
“Yashir” <yashir@yahoo.com>
2025-02-03 13:57:53 +0400

    03/02/25 update

---
+++ resources/views/hello.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Hello Page</title>
+    <h1>Hello, World!</h1>
+    <h2>Welcome to my Page!</h2>
+    <h3>This page is still under construction...</h3>
+</head>
+
+<body>
+
+</body>
+
+</html>
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HelloController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/hello', [HelloController::class, 'index']);
